f4b439c09966c4994ce022a6ab54d6c761058910
Skillz <gankstarsleague@gmail.com>
2019-10-23 14:58:26 -0400

    add setname command from yuuko core

---
+++ src/commands/setname.ts
@@ -0,0 +1,18 @@
+import { Command } from 'yuuko'
+
+export default new Command(
+  'setname',
+  async (msg, args, context) => {
+    try {
+      await context.client.editSelf({ username: args.join(' ') })
+      await msg.channel.createMessage('Username updated!')
+    } catch (err) {
+      msg.channel
+        .createMessage(`There was an error while changing username.\n\`\`\`\n${err.message}\n\`\`\``)
+        .catch(() => null)
+    }
+  },
+  {
+    owner: true
+  }
+)
