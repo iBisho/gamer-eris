af1e8515c50445b4a5ba77e111b8caceb914e811
Skillz <gankstarsleague@gmail.com>
2019-10-23 13:23:44 -0400

    add poke command

---
+++ src/commands/fun/gif/poke.ts
@@ -0,0 +1,36 @@
+import { Command } from 'yuuko'
+import fetch from 'node-fetch'
+import GamerClient from '../../../lib/structures/GamerClient'
+import GamerEmbed from '../../../lib/structures/GamerEmbed'
+import { TenorGif } from '../../../lib/types/tenor'
+
+export default new Command(`poke`, async (message, _args, context) => {
+  const language = (context.client as GamerClient).i18n.get('en-US')
+  if (!language) return null
+
+  const data: TenorGif | null = await fetch(`https://api.tenor.com/v1/search?q=poke&key=LIVDSRZULELA&limit=50`)
+    .then(res => res.json())
+    .catch(() => null)
+
+  if (!data || !data.results.length) return message.channel.createMessage(language(`fun/advice:ERROR`))
+  const randomResult = data.results[Math.floor(Math.random() * (data.results.length - 1))]
+  const [media] = randomResult.media
+
+  const user = message.mentions.length ? message.mentions[0] : message.author
+
+  const embed = new GamerEmbed()
+    .setAuthor(
+      message.member ? message.member.nick || message.member.username : message.author.username,
+      message.author.defaultAvatarURL
+    )
+    .setDescription(
+      language(user.id === message.author.id ? `fun/poke:SELF` : `fun/poke:REPLY`, {
+        mention: user.mention,
+        author: message.author.mention
+      })
+    )
+    .setImage(media.gif.url)
+    .setFooter(`Via Tenor`)
+
+  return message.channel.createMessage({ embed: embed.code })
+})
