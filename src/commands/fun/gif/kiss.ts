de6d5cb6719f6ecea51d836242fad444117b2b13
Skillz <gankstarsleague@gmail.com>
2019-10-23 13:14:40 -0400

    move typings to types folder

--- src/commands/fun/gif/kiss.ts
+++ src/commands/fun/gif/kiss.ts
@@ -2,6 +2,7 @@ import { Command } from 'yuuko'
 import fetch from 'node-fetch'
 import GamerClient from '../../../lib/structures/GamerClient'
 import GamerEmbed from '../../../lib/structures/GamerEmbed'
+import { TenorGif } from '../../../lib/types/tenor'
 
 export default new Command(`kiss`, async (message, _args, context) => {
   const language = (context.client as GamerClient).i18n.get('en-US')
@@ -33,43 +34,3 @@ export default new Command(`kiss`, async (message, _args, context) => {
 
   return message.channel.createMessage({ embed: embed.code })
 })
-
-export interface TenorGif {
-  weburl: string
-  results: TenorGifData[]
-}
-
-export interface TenorGifData {
-  tags: string[]
-  url: string
-  media: Media[]
-  created: number
-  shares: number
-  itemurl: string
-  composite: null
-  hasaudio: boolean
-  title: string
-  id: string
-}
-
-export interface Media {
-  nanomp4: MediaData
-  nanowebm: MediaData
-  tinygif: MediaData
-  tinymp4: MediaData
-  tinywebm: MediaData
-  webm: MediaData
-  gif: MediaData
-  mp4: MediaData
-  loopedmp4: MediaData
-  mediumgif: MediaData
-  nanogif: MediaData
-}
-
-export interface MediaData {
-  url: string
-  dims: number[]
-  preview: string
-  size: number
-  duration?: number
-}
---
+++ src/lib/types/tenor.ts
@@ -0,0 +1,39 @@
+export interface TenorGif {
+  weburl: string
+  results: TenorGifData[]
+}
+
+export interface TenorGifData {
+  tags: string[]
+  url: string
+  media: Media[]
+  created: number
+  shares: number
+  itemurl: string
+  composite: null
+  hasaudio: boolean
+  title: string
+  id: string
+}
+
+export interface Media {
+  nanomp4: MediaData
+  nanowebm: MediaData
+  tinygif: MediaData
+  tinymp4: MediaData
+  tinywebm: MediaData
+  webm: MediaData
+  gif: MediaData
+  mp4: MediaData
+  loopedmp4: MediaData
+  mediumgif: MediaData
+  nanogif: MediaData
+}
+
+export interface MediaData {
+  url: string
+  dims: number[]
+  preview: string
+  size: number
+  duration?: number
+}
