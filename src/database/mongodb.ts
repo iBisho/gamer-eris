25647c172e6b0fc2f32f971e9860e36f8001319b
Skillz <gankstarsleague@gmail.com>
2019-10-24 16:26:45 -0400

    add user schema

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose'
 import GuildSchema from './schemas/guild'
 import TradingCardSchema from './schemas/tradingCards'
+import UserSchema from './schemas/user'
 
 // Connect to the db
 mongoose.connect(`mongodb://localhost/:27017/test`, { useNewUrlParser: true })
@@ -11,4 +12,6 @@ db.once(`open`, () => {
   console.log(`MongoDB Connected!`)
 	const GuildModel = mongoose.model('Guild', GuildSchema)
 	const TradingCardModel = mongoose.model('TradingCards', TradingCardSchema)
+	const UserModel = mongoose.model('User', UserSchema)
+	
 })
---
+++ src/database/schemas/user.ts
@@ -0,0 +1,44 @@
+import * as mongoose from 'mongoose'
+
+export default new mongoose.Schema({
+  profile: {
+    // The id number of the background that the user has equipped
+    backgroundID: { type: Number, default: 52 },
+    // The theme color of the profile card
+    theme: { type: String, default: `white` }
+  },
+  afk: {
+    // Whether the afk message should be sent when someone @ the user
+    enabled: Boolean,
+    // The message to send when the user is afk and someone @mentions the user. Can be JSON string for embeds
+    message: { type: String, default: `Hi 👋, I am AFK at the moment. I will get back to you as soon as possible. 😄` }
+  },
+  // If you are a mod and ban a user on a server, this can allow you to easily ban that user on all servers u share.
+  moderationNetworkEnabled: Boolean,
+  vip: {
+    // Whether or not the user is a vip user
+    isVIP: Boolean,
+    // The guilds this user has registered as VIP
+    guildsRegistered: [String],
+    // The amount of tickets the user has remaining
+    openTickets: Number
+  },
+  leveling: {
+    // The current XP
+    xp: Number,
+    // The current Level
+    level: Number,
+    // The currency the user has. Used to buy backgrounds/badges
+    currency: Number,
+    // The backgrounds the user has bought.
+    backgrounds: [Number],
+    badges: {
+      // The badges the user has bought.
+      bought: [Number],
+      // The badges the user has equipped
+      equipped: [Number]
+    },
+    // The badge spots the user has bought
+    badgesUnlocked: { type: Number, min: 1, max: 4, default: 1 }
+  }
+})
