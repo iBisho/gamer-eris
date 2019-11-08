d568466e8fad0c1c3c909417d0e2964574e8a63a
Skillz <gankstarsleague@gmail.com>
2019-11-07 19:01:20 -0500

    fix personalitlies

--- src/constants/personalities.ts
+++ src/constants/personalities.ts
@@ -1,9 +1,7 @@
 export default [
-  { id: `en-US`, name: `:flag_us: English (Default Language)` },
-  { id: `pirate-US`, name: `<:pirateflag:564111649930215435> Pirate English Arr Matey!` },
-  { id: `shizzle-US`, name: `:flag_us: Gangsta English (NSFW)` }
+  { id: `en-US`, name: `:flag_us: English (Default Language)`, names: [`en-US`, `english`] },
+  { id: `fr-FR`, name: `:flag_fr: French`, names: [`fr-FR`, `french`] }
   // { id: `de-DE`, name: `:flag_de: German` },
-  // { id: `fr-FR`, name: `:flag_fr: French` }
   // { id: 'br-BR', name: 'Portue.....james spell this properly' },
   // { id: 'ge-GE', name: ':flag_de: Deutsch (DE)' }
 ]
