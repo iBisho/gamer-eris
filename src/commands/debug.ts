42f248e4779dcc47785004b43e314ce9d52f0746
Skillz <gankstarsleague@gmail.com>
2019-12-03 18:59:37 -0500

    allow only me to use debug command

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -5,44 +5,40 @@ const inspectOptions = {
   depth: 1
 }
 
-export default new Command(
-  ['debug', `eval`],
-  async function debug(message, args, context) {
-    const string = args
-      .join(' ')
-      .replace(/^\s+/, '')
-      .replace(/\s*$/, '')
+export default new Command(['debug', `eval`], async function debug(message, args, context) {
+  if (message.author.id !== '130136895395987456') return
 
-    // Allows use of Gamer in the evals
-    const Gamer = context.client
-    if (!Gamer) return
+  const string = args
+    .join(' ')
+    .replace(/^\s+/, '')
+    .replace(/\s*$/, '')
 
-    // Eval the things and send the results
-    let result
-    try {
-      result = eval(string)
-    } catch (e) {
-      result = e
-    }
-    const response = ['```js', '```']
+  // Allows use of Gamer in the evals
+  const Gamer = context.client
+  if (!Gamer) return
 
-    // We returned a promise?
-    if (result && typeof result.then === 'function') {
-      // Sweet. Wait for that to resolve.
-      let value
-      try {
-        value = util.inspect(await result, inspectOptions)
-      } catch (err) {
-        value = err
-      }
-      response.splice(1, 0, util.inspect(value, inspectOptions).substring(0, 1985))
-    } else {
-      response.splice(1, 0, String(util.inspect(result)).substring(0, 1985))
-    }
-
-    await message.channel.createMessage(response.join('\n'))
-  },
-  {
-    owner: true
+  // Eval the things and send the results
+  let result
+  try {
+    result = eval(string)
+  } catch (e) {
+    result = e
   }
-)
+  const response = ['```js', '```']
+
+  // We returned a promise?
+  if (result && typeof result.then === 'function') {
+    // Sweet. Wait for that to resolve.
+    let value
+    try {
+      value = util.inspect(await result, inspectOptions)
+    } catch (err) {
+      value = err
+    }
+    response.splice(1, 0, util.inspect(value, inspectOptions).substring(0, 1985))
+  } else {
+    response.splice(1, 0, String(util.inspect(result)).substring(0, 1985))
+  }
+
+  await message.channel.createMessage(response.join('\n'))
+})
