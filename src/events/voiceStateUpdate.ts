7806c0ce380cdd0b0b97c06ffc512c486a4e7686
Skillz <gankstarsleague@gmail.com>
2019-12-03 18:22:55 -0500

    remove unused event

--- src/events/voiceStateUpdate.ts
+++
@@ -1,8 +0,0 @@
-import { Member, OldVoiceState } from 'eris'
-import Event from '../lib/structures/Event'
-
-export default class extends Event {
-  async execute(member: Member, oldState: OldVoiceState) {
-    console.log('voicestate update event', member.id, oldState)
-  }
-}
