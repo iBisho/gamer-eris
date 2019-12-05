9f2b6326e2f1ccb85d8b1c19eca621a7579f47e8
Skillz <gankstarsleague@gmail.com>
2019-12-04 20:48:51 -0500

    add response when no events on a server

--- src/commands/events.ts
+++ src/commands/events.ts
@@ -1,7 +1,6 @@
 import { Command } from 'yuuko'
 import GamerClient from '../lib/structures/GamerClient'
 import { PrivateChannel, GroupChannel } from 'eris'
-import { GamerEvent } from '../lib/types/gamer'
 import GamerEmbed from '../lib/structures/GamerEmbed'
 
 export default new Command([`events`, `event`, `e`], async (message, _args, context) => {
@@ -9,12 +8,15 @@ export default new Command([`events`, `event`, `e`], async (message, _args, cont
 
   const Gamer = context.client as GamerClient
 
-  const events = (await Gamer.database.models.event.find({ guildID: message.channel.guild.id })) as GamerEvent[]
-  if (!events.length) return
+  const language = Gamer.i18n.get(Gamer.guildLanguages.get(message.channel.guild.id) || `en-US`)
+  if (!language) return
+
+  const events = await Gamer.database.models.event.find({ guildID: message.channel.guild.id })
+  if (!events.length) return message.channel.createMessage(language('events/events:NONE'))
 
   const embed = new GamerEmbed()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription(Gamer.helpers.events.listEvents(events))
 
-  message.channel.createMessage({ embed: embed.code })
+  return message.channel.createMessage({ embed: embed.code })
 })
