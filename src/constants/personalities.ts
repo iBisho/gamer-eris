318aef30fe24a2c16f5312ba235ddedf4943c010
Skillz <gankstarsleague@gmail.com>
2019-12-27 17:12:25 -0500

    add portuguese

--- src/constants/personalities.ts
+++ src/constants/personalities.ts
@@ -1,6 +1,6 @@
 export default [
   { id: `en-US`, name: `:flag_us: English (Default Language)`, names: [`en-US`, `english`] },
   { id: `fr_FR`, name: `:flag_fr: French`, names: [`fr_fr`, `french`, `français`] },
-  { id: `de_DE`, name: `:flag_de: German`, names: [`de_de`, `german`, `deutsch`] }
-  // { id: 'br_BR', name: 'Portue.....james spell this properly' },
+  { id: `de_DE`, name: `:flag_de: German`, names: [`de_de`, `german`, `deutsch`] },
+  { id: 'pt_BR', name: ':flag_br: Português', names: [`pt_br`, 'portuguese', 'português', 'portuguesa'] }
 ]
