587e5fa91e9085f92e3a25c7206e92cebdad1b99
Skillz <gankstarsleague@gmail.com>
2019-12-30 09:36:30 -0500

    typo fix

--- src/commands/mines.ts
+++ src/commands/mines.ts
@@ -4,16 +4,16 @@ import GamerClient from '../lib/structures/GamerClient'
 import { PrivateChannel, GroupChannel } from 'eris'
 
 export default new Command([`minesweeper`, `mines`], async (message, _args, context) => {
-	if (message.channel instanceof PrivateChannel || message.channel instanceof GroupChannel) return
+  if (message.channel instanceof PrivateChannel || message.channel instanceof GroupChannel) return
 
-	const Gamer = context.client as GamerClient
-	const language = Gamer.i18n.get(Gamer.guildLanguages.get(message.channel.guild.id) || 'en-US')
-	if (!language) return
+  const Gamer = context.client as GamerClient
+  const language = Gamer.i18n.get(Gamer.guildLanguages.get(message.channel.guild.id) || 'en-US')
+  if (!language) return
 
-	const minesweeper = new Minesweeper({ rows: 9, columns: 9, mines: 10 })
+  const minesweeper = new Minesweeper({ rows: 9, columns: 9, mines: 10 })
   const matrix = minesweeper.start()
 
   if (typeof matrix !== 'string') return
 
-  return message.channel.createMessage(`${language('gaming/minesweeper:INSTRUCTIONS')}\n${matrix})
+  return message.channel.createMessage(`${language('gaming/minesweeper:INSTRUCTIONS')}\n${matrix}`)
 })
