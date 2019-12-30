483c63ca8c307135c8da2393ea167b763acfe535
Mattis <sgtmattis@gmail.com>
2019-12-31 00:09:05 +0100

    missions

--- src/commands/8ball.ts
+++ src/commands/8ball.ts
@@ -3,12 +3,13 @@ import GamerClient from '../lib/structures/GamerClient'
 import { PrivateChannel, GroupChannel } from 'eris'
 
 export default new Command([`8ball`, `8b`, `fortune`], async (message, args, context) => {
-  if (message.channel instanceof PrivateChannel || message.channel instanceof GroupChannel) return
+  if (message.channel instanceof PrivateChannel || message.channel instanceof GroupChannel || !message.member) return
 
   const Gamer = context.client as GamerClient
   const language = Gamer.i18n.get(Gamer.guildLanguages.get(message.channel.guild.id) || 'en-US')
   if (!language) return
   if (!args.length) return message.channel.createMessage(language(`fun/8ball:NO_ARGS`))
 
-  return message.channel.createMessage(language(`fun/8ball:REPLY_NUMBER${Math.floor(Math.random() * 12)}`))
+  message.channel.createMessage(language(`fun/8ball:REPLY_NUMBER${Math.floor(Math.random() * 12)}`))
+  return Gamer.helpers.levels.completeMission(message.member, `8ball`, message.channel.guild.id)
 })
