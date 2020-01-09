803bb809295f96060754dcd03e43164153ef300a
Skillz <gankstarsleague@gmail.com>
2020-01-09 17:59:31 -0500

    add xp to all members in a role

--- src/commands/xp.ts
+++ src/commands/xp.ts
@@ -16,15 +16,37 @@ export default new Command(`xp`, async (message, args, context) => {
   const language = Gamer.i18n.get(Gamer.guildLanguages.get(message.channel.guild.id) || `en-US`)
   if (!language) return
 
-  const [type, number, userID] = args
+  const [type, number, ...idOrRoleName] = args
   const isAdding = type.toLowerCase() === `add`
   const amount = parseInt(number, 10)
   if (!amount) return
 
-  const memberID = message.mentions.length ? message.mentions[0].id : userID
+  const idOrName = idOrRoleName.join(' ').toLowerCase()
+  const memberID = message.mentions.length ? message.mentions[0].id : idOrName
 
+  const role =
+    message.channel.guild.roles.get(idOrName) ||
+    message.channel.guild.roles.find(r => r.name.toLowerCase() === idOrName)
+
+  if (role) {
+    for (const member of message.channel.guild.members.values()) {
+      if (!member.roles.includes(role.id)) continue
+      // User has the role so they need to be updated
+      if (isAdding) Gamer.helpers.levels.addLocalXP(member, amount, true)
+      else if (type.toLowerCase() === `remove`) Gamer.helpers.levels.removeXP(member, amount)
+    }
+
+    // Respond telling the user how much they gained
+    return message.channel.createMessage(
+      language(isAdding ? `leveling/xp:ADDED_ALL` : `leveling/xp:REMOVED_ALL`, {
+        rolename: role.name,
+        amount
+      })
+    )
+  }
+
+  // The user is trying to update just 1 member
   const member = message.channel.guild.members.get(memberID) || message.member
-
   if (isAdding) Gamer.helpers.levels.addLocalXP(member, amount, true)
   else if (type.toLowerCase() === `remove`) Gamer.helpers.levels.removeXP(member, amount)
   // Cancel out if not add or remove
