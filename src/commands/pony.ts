718bde0c27336b32d15b324d3a00012c4968eb0e
Skillz <gankstarsleague@gmail.com>
2020-01-15 09:18:15 -0500

    fix pony bug

--- src/commands/pony.ts
+++ src/commands/pony.ts
@@ -4,5 +4,5 @@ import constants from '../constants'
 
 export default new Command(`pony`, async (message, _args, context) => {
   const Gamer = context.client as GamerClient
-  return Gamer.helpers.tenor.randomGif(message, `pony`, constants.gifs.pony)
+  return Gamer.helpers.tenor.randomGif(message, `pony`, constants.gifs.pony, false)
 })
