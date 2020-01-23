9ba76d6a4bd0d7f9aa47cc39334d7a9c5e619e40
Skillz <gankstarsleague@gmail.com>
2020-01-23 10:46:08 -0500

    fix laugh => lmao typo

--- src/commands/lmao.ts
+++ src/commands/lmao.ts
@@ -2,7 +2,7 @@ import { Command } from 'yuuko'
 import GamerClient from '../lib/structures/GamerClient'
 import constants from '../constants'
 
-export default new Command(`lmao`, async (message, _args, context) => {
+export default new Command([`lmao`, `lol`, `laugh`], async (message, _args, context) => {
   const Gamer = context.client as GamerClient
   return Gamer.helpers.tenor.randomGif(message, `lmao`, constants.gifs.lmao, false)
 })
