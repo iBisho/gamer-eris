9d2fc308c7aedfed7b27ded3f7e65ccc92581586
Skillz <gankstarsleague@gmail.com>
2020-01-26 22:44:18 -0500

    bug fix

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -4,6 +4,9 @@ import GamerClient from '../lib/structures/GamerClient'
 
 export default class extends Event {
   async execute(guild: Guild) {
+    // To catch when discord api screws up and sends an invalid event
+    if (!guild) return
+
     const Gamer = guild.shard.client as GamerClient
 
     Gamer.amplitude.push({
