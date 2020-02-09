043100bed28c8f6996426b333e52eb43806ffde8
Skillz <gankstarsleague@gmail.com>
2020-02-09 12:37:03 -0500

    fix guild delete error

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -3,7 +3,10 @@ import { Guild } from 'eris'
 import GamerClient from '../lib/structures/GamerClient'
 
 export default class extends Event {
-  async execute(guild: Guild) {
+  async execute(guild: Guild | undefined) {
+    // Sometimes discord api doesnt send the guildcreate so this can be broken
+    if (!guild) return
+
     const Gamer = guild.shard.client as GamerClient
 
     Gamer.amplitude.push({
