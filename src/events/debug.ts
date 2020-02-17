ebb8ac579709b77b35920c23445b6f6d70ced0c8
Skillz <gankstarsleague@gmail.com>
2020-02-17 13:47:16 -0500

    add debug comment

--- src/events/debug.ts
+++ src/events/debug.ts
@@ -2,6 +2,7 @@ import Event from '../lib/structures/Event'
 
 export default class extends Event {
   async execute(text: string) {
+    // console.warn('debug event', text)
     if (text.includes('Global')) console.log(text)
   }
 }
