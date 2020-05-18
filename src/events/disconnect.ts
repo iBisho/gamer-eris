760391ba7af35b42daa893ebdaef9eba03c654bb
iBisho <ibisho.cwaemu@gmail.com>
2020-05-18 12:10:08 -0400

    Add more logging.

--- src/events/disconnect.ts
+++ src/events/disconnect.ts
@@ -1,14 +1,13 @@
-// This event is triggered once the bot is ready and online.
 import Event from '../lib/structures/Event'
 import Gamer from '../index'
 import { MessageEmbed } from 'helperis'
 
 export default class extends Event {
-  async execute(id: number) {
-    Gamer.helpers.logger.green(`[Shard READY] Shard ID ${id} is ready!`)
+  async execute() {
+    Gamer.helpers.logger.green(`[Shards DISCONNECTED] All shards are disconnected.`)
     const embed = new MessageEmbed()
-      .setColor(`#2ecc71`)
-      .setTitle(`Shard ${id} is ready`)
+      .setColor(`#e67e22`)
+      .setTitle(`All shards disconnected`)
       .setTimestamp()
     Gamer.createMessage(`680852595061162014`, { embed: embed.code })
   }
--- src/events/shardDisconnect.ts
+++ src/events/shardDisconnect.ts
@@ -1,14 +1,14 @@
-// This event is triggered once the bot is ready and online.
 import Event from '../lib/structures/Event'
 import Gamer from '../index'
 import { MessageEmbed } from 'helperis'
 
 export default class extends Event {
-  async execute(id: number) {
-    Gamer.helpers.logger.green(`[Shard READY] Shard ID ${id} is ready!`)
+  async execute(error: Error | undefined, id: number) {
+    Gamer.helpers.logger.green(`[Shard DISCONNECT] Shard ID ${id} disconnected.`)
     const embed = new MessageEmbed()
-      .setColor(`#2ecc71`)
-      .setTitle(`Shard ${id} is ready`)
+      .setColor(`#e74c3c`)
+      .setTitle(`Shard ${id} has been disconnected`)
+      .setDescription(`${error?.stack || 'Unknown Reason'}`)
       .setTimestamp()
     Gamer.createMessage(`680852595061162014`, { embed: embed.code })
   }
