b804bb7225b3aeb64135ce74c2b247c64d5d6908
Skillz <gankstarsleague@gmail.com>
2020-06-03 11:20:55 -0400

    new schema fix

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -1,6 +1,8 @@
 import { Command } from 'yuuko'
 import Gamer from '../..'
 
+const completedIDs = new Set<string>()
+
 export default new Command('schema', async message => {
   if (message.author.id !== '130136895395987456') return
 
@@ -14,7 +16,14 @@ export default new Command('schema', async message => {
     let guildCounter = 0
 
     await Gamer.helpers.utils.sleep(1)
+    counter = 0
+
     for (const member of guild.members.values()) {
+      if (completedIDs.has(`${guild.id}.${member.id}`)) {
+        guildCounter++
+        continue
+      }
+
       Gamer.database.models.roles
         .findOneAndUpdate(
           { memberID: member.id, guildID: guild.id },
@@ -23,12 +32,13 @@ export default new Command('schema', async message => {
         )
         .exec()
 
-      if (counter >= 10000) {
+      if (counter >= 1000) {
         console.log(`Finished ${guildCounter.toLocaleString()} of ${guild.memberCount.toLocaleString()}`)
-        await Gamer.helpers.utils.sleep(5)
+        await Gamer.helpers.utils.sleep(2)
         counter = 0
       }
 
+      completedIDs.add(`${guild.id}.${member.id}`)
       guildCounter++
       counter++
     }
