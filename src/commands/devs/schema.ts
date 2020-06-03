cbed96797a04455e3c2511d0a1b405c09e8d552a
Skillz <gankstarsleague@gmail.com>
2020-06-03 13:59:44 -0400

    fix mail

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -1,32 +1,19 @@
 import { Command } from 'yuuko'
 import Gamer from '../..'
 
-const completedIDs = new Set<string>()
-
 export default new Command('schema', async message => {
   if (message.author.id !== '130136895395987456') return
 
   const sortedGuilds = [...Gamer.guilds.values()].sort((a, b) => b.memberCount - a.memberCount)
   sortedGuilds.shift()
 
-  let counter = 0
-  for (const guild of sortedGuilds) {
-    if (guild.memberCount > 7500) continue
+  sortedGuilds.forEach(async guild => {
+    if (guild.memberCount > 5) return
 
-    Gamer.helpers.logger.blue(`Starting ${guild.name} with ${guild.memberCount.toLocaleString()}`)
     if (guild.memberCount !== guild.members.size) await guild.fetchAllMembers()
     Gamer.helpers.logger.green(`Finished fetching ${guild.name}`)
-    let guildCounter = 0
 
-    await Gamer.helpers.utils.sleep(1)
-    counter = 0
-
-    for (const member of guild.members.values()) {
-      if (completedIDs.has(`${guild.id}.${member.id}`)) {
-        guildCounter++
-        continue
-      }
-
+    guild.members.forEach(member => {
       Gamer.database.models.roles
         .findOneAndUpdate(
           { memberID: member.id, guildID: guild.id },
@@ -34,18 +21,8 @@ export default new Command('schema', async message => {
           { upsert: true }
         )
         .exec()
-
-      if (counter >= 2000) {
-        console.log(`Finished ${guildCounter.toLocaleString()} of ${guild.memberCount.toLocaleString()}`)
-        await Gamer.helpers.utils.sleep(2)
-        counter = 0
-      }
-
-      completedIDs.add(`${guild.id}.${member.id}`)
-      guildCounter++
-      counter++
-    }
-  }
+    })
+  })
 
   return message.channel.createMessage('done updating schema')
 })
