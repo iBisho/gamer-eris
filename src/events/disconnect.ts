0af3fed51a5689999112b6ab4e1f682a842a0885
Skillz <gankstarsleague@gmail.com>
2020-06-08 12:49:22 -0400

    test fix

--- src/events/disconnect.ts
+++ src/events/disconnect.ts
@@ -6,5 +6,5 @@ import constants from '../constants'
 export default new EventListener('disconnect', () => {
   Gamer.helpers.logger.debug(`[Shards DISCONNECTED] All shards are disconnected.`)
   const embed = new MessageEmbed().setColor(`#e67e22`).setTitle(`All shards disconnected`).setTimestamp()
-  if (Gamer?.user.id === constants.general.gamerID) Gamer.createMessage(`680852595061162014`, { embed: embed.code })
+  if (Gamer?.user?.id === constants.general.gamerID) Gamer.createMessage(`680852595061162014`, { embed: embed.code })
 })
