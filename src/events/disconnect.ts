b3ee7390f5d50a75b9fa803f7d1c6a06b2ae0fac
Skillz <gankstarsleague@gmail.com>
2020-06-08 12:46:03 -0400

    test fix

--- src/events/disconnect.ts
+++ src/events/disconnect.ts
@@ -4,8 +4,7 @@ import { EventListener } from 'yuuko'
 import constants from '../constants'
 
 export default new EventListener('disconnect', () => {
-  if (Gamer.user.id !== constants.general.gamerID) return
   Gamer.helpers.logger.debug(`[Shards DISCONNECTED] All shards are disconnected.`)
   const embed = new MessageEmbed().setColor(`#e67e22`).setTitle(`All shards disconnected`).setTimestamp()
-  Gamer.createMessage(`680852595061162014`, { embed: embed.code })
+  if (Gamer?.user.id === constants.general.gamerID) Gamer.createMessage(`680852595061162014`, { embed: embed.code })
 })
--- src/events/shardDisconnect.ts
+++ src/events/shardDisconnect.ts
@@ -1,11 +1,11 @@
 import Gamer from '../index'
 import { MessageEmbed } from 'helperis'
 import { EventListener } from 'yuuko'
-import constants from '../constants/index'
-export default new EventListener('shardDisconnect', (error, id) => {
-  if (Gamer.user.id !== constants.general.gamerID) return
 
+export default new EventListener('shardDisconnect', (error, id) => {
   Gamer.helpers.logger.debug(`[Shard DISCONNECT] Shard ID ${id} disconnected.`)
+  if (!Gamer.getChannel(`680852595061162014`)) return
+
   const embed = new MessageEmbed()
     .setColor(`#e74c3c`)
     .setTitle(`Shard ${id} has been disconnected`)
