4153d8483edc993604523f751c002b05f9eb4afc
Skillz <gankstarsleague@gmail.com>
2020-06-11 14:04:31 -0400

    test fix events

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -1,13 +1,31 @@
 import { Command } from 'yuuko'
 import Gamer from '../..'
+import { TextChannel, NewsChannel } from 'eris'
 
 export default new Command('schema', async message => {
-  const guilds = await Gamer.database.models.guild.find()
-  for (const guild of guilds) {
-    if (typeof guild.id === 'string') guild.guildID = guild.id
-    guild.save()
+  const events = await Gamer.database.models.event.find()
+  for (const event of events) {
+    if (event.adChannelID) {
+      const channel = Gamer.getChannel(event.adChannelID)
+      if (channel) {
+        if (channel instanceof TextChannel || channel instanceof NewsChannel) {
+          event.guildID = channel.guild.id
+        }
+      }
+    } else if (event.alertRoleIDs.length) {
+      const guild = Gamer.guilds.find(guild => event.alertRoleIDs.some(id => guild.roles.has(id)))
+      if (guild) {
+        event.guildID = guild.id
+      }
+    } else if (event.allowedRoleIDs.length) {
+      const guild = Gamer.guilds.find(guild => event.allowedRoleIDs.some(id => guild.roles.has(id)))
+      if (guild) {
+        event.guildID = guild.id
+      }
+    }
+
+    event.save()
   }
-  message.channel.createMessage('guilds are updated')
 
   return message.channel.createMessage('done updating schema')
 })
