9a0b5296467eafeb71239f93c9cf84af506f2216
Skillz <gankstarsleague@gmail.com>
2020-06-11 15:14:09 -0400

    allow reload updates

--- src/commands/devs/update.ts
+++ src/commands/devs/update.ts
@@ -1,13 +1,23 @@
 import { Command } from 'yuuko'
 import { promisify } from 'util'
 import { exec } from 'child_process'
+import Gamer from '../..'
 
 const asyncExecute = promisify(exec)
 
-export default new Command('update', async message => {
+export default new Command('update', async (message, args, context) => {
   if (message.author.id !== '130136895395987456') return
 
-  await asyncExecute('git pull && npm run build && pm2 restart gamer')
+  const [type] = args
+  switch (type) {
+    case 'reload':
+      await asyncExecute('git pull')
+      const reloadCommand = Gamer.commandForName('reload')
+      reloadCommand?.execute(message, [], context)
+      break
+    default:
+      await asyncExecute('git pull && npm run build && pm2 restart gamer')
+  }
 
   message.channel.createMessage('Git pulled, recompiled and restarted the bot. Bot should be starting up as we speak.')
 })
