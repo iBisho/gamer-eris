30229e5d02c89efd3b51caf93c1254b0cb5eb313
Skillz <gankstarsleague@gmail.com>
2020-06-11 09:54:03 -0400

    guild schema

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -2,29 +2,12 @@ import { Command } from 'yuuko'
 import Gamer from '../..'
 
 export default new Command('schema', async message => {
-  const events = await Gamer.database.models.event.find()
-  for (const event of events) {
-    if (typeof event.id === 'number') event.eventID = event.id
-    event.save()
+  const guilds = await Gamer.database.models.guild.find()
+  for (const guild of guilds) {
+    if (typeof guild.id === 'string') guild.guildID = guild.id
+    guild.save()
   }
-  message.channel.createMessage('events are updated')
-
-  const mails = await Gamer.database.models.mail.find()
-  for (const mail of mails) {
-    mail.channelID = mail.id
-  }
-  message.channel.createMessage('mails are updated')
-
-  const feedbacks = await Gamer.database.models.feedback.find()
-  for (const fb of feedbacks) {
-    fb.feedbackID = fb.id
-  }
-  message.channel.createMessage('reminders are updated')
-
-  const reminders = await Gamer.database.models.reminder.find()
-  for (const rem of reminders) {
-    rem.reminderID = rem.id
-  }
+  message.channel.createMessage('guilds are updated')
 
   return message.channel.createMessage('done updating schema')
 })
