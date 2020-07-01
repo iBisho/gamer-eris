ed5646bd454ce90059a06c371cafe1735bbeaa2a
Skillz <gankstarsleague@gmail.com>
2020-07-01 10:58:30 -0400

    fix typo

--- src/constants/profiles.ts
+++ src/constants/profiles.ts
@@ -121,87 +121,104 @@ export default {
     {
       id: 1,
       name: `SadAnimeGirly - Submitted By GeheimerWolf#8008`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/SadAnimeGirly.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/SadAnimeGirly.png`)),
+      vipNeeded: false
     },
     {
       id: 2,
       name: `BF1 Assault`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/bf1pilot.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/bf1pilot.jpg`)),
+      vipNeeded: false
     },
     {
       id: 3,
       name: `unOrdinary`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/unordinary.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/unordinary.jpg`)),
+      vipNeeded: false
     },
     {
       id: 4,
       name: `Free Fire 1`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire1.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire1.png`)),
+      vipNeeded: false
     },
     {
       id: 5,
       name: `Free Fire 2`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire2.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire2.png`)),
+      vipNeeded: false
     },
     {
       id: 6,
       name: `Free Fire 3`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire3.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/freefire3.png`)),
+      vipNeeded: false
     },
     {
       id: 7,
       name: `ATS - Submitted By BeardCaliper#1897`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/ats.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/ats.jpg`)),
+      vipNeeded: false
     },
     {
       id: 8,
       name: `Dicey Cafe - Submitted By Derk#0127`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/diceycafe.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/diceycafe.png`)),
+      vipNeeded: false
     },
     {
       id: 9,
       name: `Control`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/control.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/control.jpg`)),
+      vipNeeded: false
     },
     {
       id: 10,
       name: `Ark Survival Evolved - Submitted By Aikage#4444`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/ark.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/ark.jpg`)),
+      vipNeeded: false
     },
     {
       id: 11,
       name: `PlayerUnknown's Battlegrounds - Submitted By GeheimerWolf#8008`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/pubg.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/pubg.png`)),
+      vipNeeded: false
     },
     {
       id: 12,
       name: `Shop Titans`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/shoptitans.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/shoptitans.jpg`)),
+      vipNeeded: false
     },
     {
       id: 13,
       name: `Minecraft - Submitted By Aikage#4444`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/minecraft.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/minecraft.png`)),
+      vipNeeded: false
     },
     {
       id: 14,
       name: `CodeVein - Submitted By Aikage#4444`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/CodeVein.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/CodeVein.jpg`)),
+      vipNeeded: false
     },
     {
       id: 15,
       name: `Final-Fantasy-VII - Submitted By Aikage#4444`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/Final-Fantasy-VII.jpg`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/Final-Fantasy-VII.jpg`)),
+      vipNeeded: false
     },
     {
       id: 16,
       name: `Something - Submitted By Aikage#4444`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/something.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/something.png`)),
+      vipNeeded: false
     },
     {
       id: 17,
       name: `Overwatch - Submitted By GeheimerWolf#8008`,
-      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/Overwatch.png`))
+      buffer: fs.readFileSync(join(assetsFolder, `profile/Backgrounds/Overwatch.png`)),
+      vipNeeded: false
     }
   ],
   backgroundPackages: [],
