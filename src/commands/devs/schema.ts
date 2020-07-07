320aa7c3291da115aad5e81ec30c2e3bfaa0fa8a
Skillz <gankstarsleague@gmail.com>
2020-07-07 14:43:47 -0400

    test fix user db

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -3,37 +3,64 @@ import { Command } from 'yuuko'
 import Gamer from '../..'
 
 export default new Command('schema', async message => {
-  let counter = 0
-  const settings = await Gamer.database.models.user.find()
-  for (const setting of settings) {
-    const payload = {
-      userID: setting.userID,
-      guildIDs: setting.guildIDs,
-      // @ts-ignore
-      backgroundID: setting.profile.backgroundID,
-      // @ts-ignore
-      theme: setting.profile.theme,
-      // @ts-ignore
-      afkEnabled: setting.afk.enabled,
-      // @ts-ignore
-      afkMessage: setting.afk.message,
-      // @ts-ignore
-      isVIP: setting.vip.isVIP,
-      // @ts-ignore
-      vipGuildsRegistered: setting.vip.guildsRegistered,
-      // @ts-ignore
-      boosts: setting.leveling.boosts,
-      // @ts-ignore
-      xp: setting.leveling.xp,
-      // @ts-ignore
-      currency: setting.leveling.currency,
-      // @ts-ignore
-      networkGuildID: setting.network.guildID
+  let mainCounter = 0
+
+  let xpLimit = 10000
+
+  let searching = true
+  while (searching) {
+    const settings = await Gamer.database.models.user.find({ 'leveling.xp': { $gt: xpLimit } })
+    console.log('found settings amount:', settings.length, xpLimit)
+
+    if (!settings.length) {
+      if (xpLimit === 0) {
+        searching = false
+        break
+      }
+
+      xpLimit -= 1000
+      console.log('lowering xplimit to:', xpLimit)
+      continue
     }
 
-    await Gamer.database.models.user.replaceOne({ userID: payload.userID }, payload).exec()
-    counter++
-    console.log(`Completed ${counter} / ${settings.length}`)
+    console.log(settings)
+
+    let counter = 0
+
+    for (const setting of settings) {
+      const payload = {
+        userID: setting.userID,
+        guildIDs: setting.guildIDs,
+        // @ts-ignore
+        backgroundID: setting.profile.backgroundID,
+        // @ts-ignore
+        theme: setting.profile.theme,
+        // @ts-ignore
+        afkEnabled: setting.afk.enabled,
+        // @ts-ignore
+        afkMessage: setting.afk.message,
+        // @ts-ignore
+        isVIP: setting.vip.isVIP,
+        // @ts-ignore
+        vipGuildsRegistered: setting.vip.guildsRegistered,
+        // @ts-ignore
+        boosts: setting.leveling.boosts,
+        // @ts-ignore
+        xp: setting.leveling.xp,
+        // @ts-ignore
+        currency: setting.leveling.currency,
+        // @ts-ignore
+        networkGuildID: setting.network.guildID
+      }
+
+      await Gamer.database.models.user.deleteOne({ _id: setting._id })
+      await Gamer.database.models.user.create(payload)
+
+      counter++
+      mainCounter++
+      console.log(`Completed ${counter} / ${settings.length} with limit ${xpLimit}`)
+      console.log('Main Counter:', mainCounter)
+    }
   }
 
   return message.channel.createMessage('done updating schema')
