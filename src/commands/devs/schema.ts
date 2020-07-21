ae62af3c9f40a8ec2b58a2a09e6cb9d3af8d0aa4
Skillz <gankstarsleague@gmail.com>
2020-07-21 11:41:40 -0400

    test schema

--- src/commands/devs/schema.ts
+++ src/commands/devs/schema.ts
@@ -3,62 +3,24 @@ import { Command } from 'yuuko'
 import Gamer from '../..'
 
 export default new Command('schema', async message => {
-  let mainCounter = 0
+  const records = await Gamer.database.models.spy.find()
 
-  let xpLimit = 1000
+  await Gamer.database.models.spy.deleteMany({}).exec()
 
-  let searching = true
-  while (searching) {
-    const settings = await Gamer.database.models.user.find({ 'leveling.xp': { $gt: xpLimit } })
-    console.log('found settings amount:', settings.length, xpLimit)
+  for (const record of records) {
+    const exists = await Gamer.database.models.spy.findOne({ memberID: record.memberID })
 
-    if (!settings.length) {
-      if (xpLimit === 0) {
-        searching = false
-        break
+    if (exists) {
+      for (const word of record.words) {
+        await Gamer.database.models.spy
+          .findOneAndUpdate({ memberID: record.memberID }, { $addToSet: { words: word.toLowerCase() } })
+          .exec()
       }
-
-      xpLimit -= 50
-      console.log('lowering xplimit to:', xpLimit)
-      continue
-    }
-
-    let counter = 0
-
-    for (const setting of settings) {
-      const payload = {
-        userID: setting.userID,
-        guildIDs: setting.guildIDs,
-        // @ts-ignore
-        backgroundID: setting.profile.backgroundID,
-        // @ts-ignore
-        theme: setting.profile.theme,
-        // @ts-ignore
-        afkEnabled: setting.afk.enabled,
-        // @ts-ignore
-        afkMessage: setting.afk.message,
-        // @ts-ignore
-        isVIP: setting.vip.isVIP,
-        // @ts-ignore
-        vipGuildsRegistered: setting.vip.guildsRegistered,
-        // @ts-ignore
-        boosts: setting.leveling.boosts,
-        // @ts-ignore
-        xp: setting.leveling.xp,
-        // @ts-ignore
-        currency: setting.leveling.currency,
-        // @ts-ignore
-        networkGuildID: setting.network.guildID
-      }
-
-      await Gamer.database.models.user.deleteOne({ _id: setting._id })
-      // @ts-ignore
-      await Gamer.database.models.user.create(payload)
-
-      counter++
-      mainCounter++
-      console.log(`Completed ${counter} / ${settings.length} with limit ${xpLimit}`)
-      console.log('Main Counter:', mainCounter)
+    } else {
+      await Gamer.database.models.spy.create({
+        memberID: record.memberID,
+        words: record.words.map(w => w.toLowerCase())
+      })
     }
   }
 
