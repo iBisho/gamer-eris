7a1eea160301918d72a55f8dcbaf548327b7bbf0
Skillz <gankstarsleague@gmail.com>
2020-07-27 11:34:22 -0400

    refactor

--- src/commands/vip/resetanalyze.ts
+++ src/commands/vip/resetanalyze.ts
@@ -1,35 +1,26 @@
 import { Command } from 'yuuko'
 import GamerClient from '../../lib/structures/GamerClient'
-import { upsertGuild } from '../../database/mongoHandler'
+import { sendMessage } from '../../lib/utils/eris'
 
-export default new Command([`resetanalyze`, `resetanalytics`, `analyzereset`, `analyticsreset`], async (message, _args, context) => {
-  const Gamer = context.client as GamerClient
+export default new Command(
+  [`resetanalyze`, `resetanalytics`, `analyzereset`, `analyticsreset`],
+  async (message, _args, context) => {
+    if (!message.member) return
 
-  if (!message.member) return
+    const Gamer = context.client as GamerClient
 
+    const guildID = message.member.guild.id
+    const language = Gamer.getLanguage(guildID)
 
-const guildID = message.member.guild.id
-const language = Gamer.getLanguage(guildID)
-
-
-  {
     const guildSettings = await Gamer.database.models.guild.findOne({ guildID: message.member.guild.id })
-
-    if (!guildSettings?.vip.isVIP) return message.channel.createMessage(language(`vip/analyze:NEED_VIP`))
+    if (!guildSettings?.vip.isVIP) return sendMessage(message.channel.id, language(`vip/analyze:NEED_VIP`))
 
     // If the user is not an admin/mod cancel out
     if (!Gamer.helpers.discord.isModOrAdmin(message, guildSettings))
-     return message.channel.createMessage(language('common:NOT_MOD_OR_ADMIN'))
+      return sendMessage(message.channel.id, language('common:NOT_MOD_OR_ADMIN'))
+
+    Gamer.database.models.analytics.deleteMany({ guildID }).exec()
+
+    return sendMessage(message.channel.id, language(`vip/resetanalyze:ANALYZE_RESETTED`))
   }
-
-  const guildSettings = await upsertGuild(message.member.guild.id)
-  // Only mods or admins can delete analytics
-  Gamer.database.models.analytics.deleteMany({ guildID }).exec()
-
-  // If the bot should reset the VIP
-  guildSettings.vip.isVIP = true
-  guildSettings.save()
-
-
-  return message.channel.createMessage(language(`vip/resetanalyze:ANALYZE_RESETTED`))
-})
+)
