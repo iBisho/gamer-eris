c50c2f222c51d6878d13b92036d07c4ee95241b6
Skillz <gankstarsleague@gmail.com>
2020-08-15 11:34:01 -0400

    fix urban error

--- src/commands/fun/urban.ts
+++ src/commands/fun/urban.ts
@@ -23,7 +23,7 @@ export default new Command(`urban`, async (message, args, context) => {
     .then(res => res.json())
     .catch(() => [])) as Urban
 
-  const [highestRated] = data.list.sort((a, b) => b.thumbs_up - a.thumbs_up)
+  const [highestRated] = data.list?.sort((a, b) => b.thumbs_up - a.thumbs_up) || []
   if (!highestRated) return
 
   const embed = new MessageEmbed()
